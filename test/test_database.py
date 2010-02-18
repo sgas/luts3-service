74acd3abfa087db6a484e069f7ee52deba97de9f
Henrik Thostrup Jensen <htj@ndgf.org>
2010-02-18 12:08:04 +0000

    make test dir into a python module and put test usage records into seperate file (for easier reuse)

--- test/test_database.py
+++ test/test_database.py
@@ -13,77 +13,12 @@ from twisted.internet import defer
 
 from sgas.server import couchdb, database, usagerecord
 
+from . import ur
+
 
 
 SGAS_TEST_FILE = os.path.join(os.path.expanduser('~'), '.sgas-test')
 
-
-UR1 = """<?xml version="1.0" encoding="UTF-8" ?>
-<ur:JobUsageRecord xmlns:ur="http://schema.ogf.org/urf/2003/09/urf">
-    <ur:RecordIdentity ur:createTime="2009-07-07T09:06:52Z" ur:recordId="gsiftp://example.org/jobs/1" />
-    <ur:JobIdentity>
-        <ur:GlobalJobId>gsiftp://example.org/jobs/1</ur:GlobalJobId>
-    </ur:JobIdentity>
-    <ur:UserIdentity>
-        <ur:GlobalUserName>/O=Grid/O=NorduGrid/OU=ndgf.org/CN=Test User</ur:GlobalUserName>
-    </ur:UserIdentity>
-    <ur:JobName>test job 1</ur:JobName>
-    <ur:MachineName>benedict.grid.aau.dk</ur:MachineName>
-    <ur:StartTime>2009-07-07T09:06:37Z</ur:StartTime>
-    <ur:EndTime>2009-07-07T09:06:52Z</ur:EndTime>
-    <ur:WallDuration>PT1S</ur:WallDuration>
-</ur:JobUsageRecord>
-"""
-
-UR2 = """<?xml version="1.0" encoding="UTF-8" ?>
-<ur:JobUsageRecord xmlns:ur="http://schema.ogf.org/urf/2003/09/urf">
-    <ur:RecordIdentity ur:createTime="2009-07-07T09:06:52Z" ur:recordId="gsiftp://example.org/jobs/2" />
-    <ur:JobIdentity>
-        <ur:GlobalJobId>gsiftp://example.org/jobs/2</ur:GlobalJobId>
-    </ur:JobIdentity>
-    <ur:UserIdentity>
-        <ur:GlobalUserName>/O=Grid/O=NorduGrid/OU=ndgf.org/CN=Test User</ur:GlobalUserName>
-    </ur:UserIdentity>
-    <ur:JobName>test job 2</ur:JobName>
-    <ur:MachineName>benedict.grid.aau.dk</ur:MachineName>
-    <ur:StartTime>2009-07-08T09:06:37Z</ur:StartTime>
-    <ur:EndTime>2009-07-08T09:06:52Z</ur:EndTime>
-    <ur:WallDuration>PT2S</ur:WallDuration>
-</ur:JobUsageRecord>
-"""
-
-CUR = """<?xml version="1.0" encoding="UTF-8" ?>
-<ur:UsageRecords xmlns:ur="http://schema.ogf.org/urf/2003/09/urf">
-  <ur:JobUsageRecord>
-    <ur:RecordIdentity ur:createTime="2009-07-07T09:06:52Z" ur:recordId="gsiftp://example.org/jobs/3" />
-    <ur:JobIdentity>
-        <ur:GlobalJobId>gsiftp://example.org/jobs/3</ur:GlobalJobId>
-    </ur:JobIdentity>
-    <ur:UserIdentity>
-        <ur:GlobalUserName>/O=Grid/O=NorduGrid/OU=ndgf.org/CN=Test User</ur:GlobalUserName>
-    </ur:UserIdentity>
-    <ur:JobName>test job 3</ur:JobName>
-    <ur:MachineName>benedict.grid.aau.dk</ur:MachineName>
-    <ur:StartTime>2009-07-08T09:06:37Z</ur:StartTime>
-    <ur:EndTime>2009-07-08T09:06:52Z</ur:EndTime>
-    <ur:WallDuration>PT3S</ur:WallDuration>
-  </ur:JobUsageRecord>
-  <ur:JobUsageRecord>
-    <ur:RecordIdentity ur:createTime="2009-07-07T09:06:52Z" ur:recordId="gsiftp://example.org/jobs/4" />
-    <ur:JobIdentity>
-        <ur:GlobalJobId>gsiftp://example.org/jobs/4</ur:GlobalJobId>
-    </ur:JobIdentity>
-    <ur:UserIdentity>
-        <ur:GlobalUserName>/O=Grid/O=NorduGrid/OU=ndgf.org/CN=Test User</ur:GlobalUserName>
-    </ur:UserIdentity>
-    <ur:JobName>test job 4</ur:JobName>
-    <ur:MachineName>benedict.grid.aau.dk</ur:MachineName>
-    <ur:StartTime>2009-07-08T09:06:37Z</ur:StartTime>
-    <ur:EndTime>2009-07-08T09:06:52Z</ur:EndTime>
-    <ur:WallDuration>PT4S</ur:WallDuration>
-  </ur:JobUsageRecord>
-</ur:UsageRecords>
-"""
 
 
 class DatabaseTest(unittest.TestCase):
@@ -111,7 +46,7 @@ class DatabaseTest(unittest.TestCase):
         ur1_id = 'gsiftp://example.org/jobs/1'
         ur1_hs = usagerecord.createID(ur1_id)
 
-        doc_ids = yield self.ur_db.insertUsageRecords(UR1)
+        doc_ids = yield self.ur_db.insertUsageRecords(ur.UR1)
         self.failUnlessEqual(doc_ids, {ur1_id: {'id':ur1_hs}})
 
         doc = yield self.ur_db.getUsageRecord(ur1_hs)
@@ -127,7 +62,7 @@ class DatabaseTest(unittest.TestCase):
         cur_hs1 = usagerecord.createID(cur_id1)
         cur_hs2 = usagerecord.createID(cur_id2)
 
-        doc_ids = yield self.ur_db.insertUsageRecords(CUR)
+        doc_ids = yield self.ur_db.insertUsageRecords(ur.CUR)
         self.failUnlessEqual(len(doc_ids), 2)
         wanted_result = {cur_id1: {'id':cur_hs1}, cur_id2: {'id':cur_hs2}}
         self.failUnlessEqual(doc_ids, wanted_result)
