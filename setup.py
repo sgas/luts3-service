7f9171eb975e2200d399e13ec869ac7ff1c00973
Henrik Thostrup Jensen <htj@ndgf.org>
2010-03-09 09:45:33 +0000

    set version number in trunk after tagging luts service 3.0.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ cmdclasses = {'install_data': InstallSGASData}
 
 
 setup(name='sgas-luts-service',
-      version='3.0.1-svn',
+      version='3.0.2-svn',
       description='SGAS LUTS Accounting Server',
       author='Henrik Thostrup Jensen',
       author_email='htj@ndgf.org',
