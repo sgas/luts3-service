4383b6e82d8d20bcb716d703f370b9883306903c
Henrik Thostrup Jensen <htj@ndgf.org>
2010-06-14 12:20:46 +0000

    use COALESCE in updater (simplifies update query quite a bit) and remove AS names (not usefull anymore)

--- sgas/database/postgresql/updater.py
+++ sgas/database/postgresql/updater.py
@@ -28,44 +28,33 @@ DELETE_AGGREGATED_INFO = '''DELETE FROM uraggregated WHERE insert_time = %s AND 
 # a nice small insert statement
 UPDATE_AGGREGATED_INFO = '''
 INSERT INTO uraggregated SELECT
-    CASE WHEN end_time IS NULL
-        THEN create_time::DATE
-        ELSE end_time::DATE
-    END                                 AS execution_date,
-    insert_time::DATE                   AS insert_date,
+    COALESCE(end_time::DATE, create_time::DATE),
+    insert_time::DATE,
     machine_name,
-    CASE WHEN global_user_name ISNULL
-        THEN (machine_name || ':' || local_user_id)
-        ELSE global_user_name
-    END                                 AS user_identity,
+    COALESCE(global_user_name, machine_name || ':' || local_user_id),
     CASE WHEN vo_issuer LIKE 'file:///%%'
         THEN NULL
         ELSE vo_issuer
-    END                                 AS vo_issuer,
+    END,
     CASE WHEN vo_name LIKE '/%%'
         THEN NULL
         ELSE vo_name
-    END                                 AS vo_name,
-    vo_attributes[1][1]                 AS vo_group,
-    vo_attributes[1][2]                 AS vo_role,
-    count(*)                            AS n_jobs,
-    SUM(COALESCE(cpu_duration,0))  / 3600.0
-                                        AS sum_cputime,
-    SUM(COALESCE(wall_duration,0)) / 3600.0
-                                        AS sum_walltime,
-    now()                               AS generate_time
+    END,
+    vo_attributes[1][1],
+    vo_attributes[1][2],
+    count(*),
+    SUM(COALESCE(cpu_duration,0))  / 3600.0,
+    SUM(COALESCE(wall_duration * node_count,0)) / 3600,
+    now()
 FROM
     usagerecords
 WHERE
     insert_time::date = %s AND machine_name = %s
 GROUP BY
-    CASE WHEN end_time IS NULL THEN create_time::DATE ELSE end_time::DATE END,
+    COALESCE(end_time::DATE, create_time::DATE),
     insert_time::DATE,
     machine_name,
-    CASE WHEN global_user_name ISNULL
-        THEN (machine_name || ':' || local_user_id)
-        ELSE global_user_name
-    END,
+    COALESCE(global_user_name, machine_name || ':' || local_user_id),
     vo_issuer,
     vo_name,
     vo_attributes
