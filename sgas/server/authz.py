9f1cb1783c9c4687e03dd242748a210791b008cb
Henrik Thostrup Jensen <htj@ndgf.org>
2010-07-13 14:12:44 +0000

    update/refactor authz module for new simpler view authz mechanism

--- sgas/server/authz.py
+++ sgas/server/authz.py
@@ -11,7 +11,6 @@ from twisted.python import log
 INSERT    = 'insert'
 RETRIEVAL = 'retrieval'
 VIEW      = 'view'
-VIEW_PREFIX = 'view:'
 
 # regular expression for matching authz lines
 rx = re.compile("""\s*"(.*)"\s*(.*)""")
@@ -27,6 +26,10 @@ def getSubject(request):
             #print x509.get_subject().get_components()
             subject = '/' + '/'.join([ '='.join(c) for c in x509.get_subject().get_components() ])
             return subject
+
+    # identity forwarded by reverse proxy
+    if request.getClientIP() == '127.0.0.1' and 'x-ssl-subject' in request.received_headers:
+        return request.received_headers.get('x-ssl-subject')
 
     # request wasn't secure or no certificate was presented
     return None
@@ -64,23 +67,19 @@ class Authorizer:
         return self.allowed_actions.get(subject, [])
 
 
-    def isAllowed(self, subject, action, view_name=None, context=None):
+    def isAllowed(self, subject, action, context=None):
 
         allowed = False
 
         try:
             if action in self.allowed_actions[subject]:
                 allowed = True
-            # check views
-            elif action == VIEW and view_name:
-                if VIEW_PREFIX + view_name in self.allowed_actions[subject]:
-                    allowed = True
-                elif context is not None and VIEW_PREFIX + view_name + ':' + context in self.allowed_actions[subject]:
-                    allowed = True
+            if context is not None and action + ':' + context in self.allowed_actions[subject]:
+                allowed = True
         except KeyError, e:
             pass
 
-        log.msg("Authz check: Subject %s, Action %s, View name: %s, Context: %s. Access allowed: %s" % \
+        log.msg("Authz check: Subject %s, Action %s, Context: %s. Access allowed: %s" % \
                  (subject, action, view_name, context, allowed), system='sgas.Authorizer')
 
         return allowed
