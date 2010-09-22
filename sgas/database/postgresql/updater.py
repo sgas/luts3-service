7d854a3c23c707915e3546d83af6b4df1cf19500
Henrik Thostrup Jensen <htj@ndgf.org>
2010-09-22 08:20:28 +0000

    new updater for using stored function for updating aggregated data

--- sgas/database/postgresql/updater.py
+++ sgas/database/postgresql/updater.py
@@ -19,49 +19,11 @@ import psycopg2
 from twisted.python import log
 from twisted.internet import defer, reactor
 from twisted.application import service
+from twisted.enterprise import adbapi
 
 
 
 SQL_SERIALIZABLE_TRANSACTION = '''SET TRANSACTION ISOLATION LEVEL SERIALIZABLE'''
-
-AGGREGATE_UPDATE_QUERY = '''SELECT * FROM uraggregated_update ORDER BY insert_time LIMIT 1'''
-
-DELETE_AGGREGATED_UPDATE = '''DELETE FROM uraggregated_update WHERE insert_time = %s AND machine_name = %s'''
-DELETE_AGGREGATED_INFO = '''DELETE FROM uraggregated WHERE insert_time = %s AND machine_name = %s'''
-
-# a nice small insert statement
-UPDATE_AGGREGATED_INFO = '''
-INSERT INTO uraggregated
-SELECT
-    COALESCE(end_time::DATE, create_time::DATE)                      AS s_execute_time,
-    insert_time::DATE                                                AS s_insert_time,
-    machine_name                                                     AS s_machine_name,
-    COALESCE(global_user_name, machine_name || ':' || local_user_id) AS s_user_identity,
-    CASE WHEN vo_issuer LIKE 'file:///%%'
-        THEN NULL
-        ELSE vo_issuer
-    END                                                              AS s_vo_issuer,
-    CASE WHEN vo_name is NULL
-        THEN COALESCE(machine_name || ':' || project_name)
-        ELSE
-            CASE WHEN vo_name LIKE '/%%'
-                THEN NULL
-                ELSE vo_name
-            END
-    END                                                              AS s_vo_name,
-    vo_attributes[1][1]                                              AS s_vo_group,
-    vo_attributes[1][2]                                              AS s_vo_role,
-    count(*)                                                         AS s_n_jobs,
-    SUM(COALESCE(cpu_duration,0))  / 3600.0                          AS s_cputime,
-    SUM(COALESCE(wall_duration,0) * COALESCE(processors,1)) / 3600.0 AS s_walltime,
-    now()                                                            AS s_generate_time
-FROM
-    usagerecords
-WHERE
-    insert_time::date = %s AND machine_name = %s
-GROUP BY
-    s_execute_time, s_insert_time, s_machine_name, s_user_identity, s_vo_issuer, s_vo_name, s_vo_group, s_vo_role;
-'''
 
 
 
@@ -125,31 +87,6 @@ class AggregationUpdater(service.Service):
 
     # -- end scheduling logic
 
-    def _performUpdate(self, txn):
-        # This is performed outside the main Twisted thread and runs in a
-        # blocking manner # i.e., no deferreds should be used.
-        # Furthermore the function is run within a transaction, so if any
-        # errors occurs everything is rolled back.
-
-        # FIXME make a pg/plsql function for this, to avoid ping-ponging
-        # with the database
-
-        txn.execute(SQL_SERIALIZABLE_TRANSACTION)
-        txn.execute(AGGREGATE_UPDATE_QUERY)
-        rows = txn.fetchall()
-        if not rows:
-            return # nothin to do
-
-        assert len(rows) == 1, 'Multiple rows returned for LIMIT 1 query'
-        insert_date, machine_name = rows[0]
-        insert_date = str(insert_date)
-
-        txn.execute(DELETE_AGGREGATED_INFO, (insert_date, machine_name))
-        txn.execute(UPDATE_AGGREGATED_INFO, (insert_date, machine_name))
-        txn.execute(DELETE_AGGREGATED_UPDATE, (insert_date, machine_name))
-        return insert_date, machine_name
-
-
     @defer.inlineCallbacks
     def update(self):
         # will update the parts of the aggregated data table which has been
@@ -159,28 +96,39 @@ class AggregationUpdater(service.Service):
         self.has_reconnected = False
 
         try:
+            conn = adbapi.Connection(self.pool_proxy.dbpool)
             while True:
                 try:
-                    idmn = yield self.pool_proxy.dbpool.runInteraction(self._performUpdate)
-                    if idmn:
-                        insert_date, machine_name = idmn
-                        log.msg('Aggregation updated: %s / %s' % (insert_date, machine_name))
-                    else: # no more updates to perform
+                    txn = adbapi.Transaction(self, conn)
+
+                    # the update_uraggregate function requires serializable isolation level
+                    # in order to execute correctly
+                    txn.execute(SQL_SERIALIZABLE_TRANSACTION)
+                    yield txn.callproc('update_uraggregate')
+                    idmn = txn.fetchall()
+                    txn.close()
+                    conn.commit()
+
+                    if idmn in (None, [(None,)]): # empty array -> response when all done
                         break
+                    else:
+                        insert_date, machine_name = idmn[0][0]
+                        log.msg('Aggregation updated: %s / %s' % (insert_date, machine_name))
                     if self.stopping:
                         break
-                except psycopg2.OperationalError, e:
-                    # if the transaction fails due to closed connection, it will cause an interface error
-                    # however dbapi will try to rollback, which will wrap/cause OperationalError instead
-                    # FIXME in the future the updating should be done in pgplsql function to avoid this
+                except psycopg2.InterfaceError, e:
+                    # typically means we lost the connection due to a db restart
                     if self.has_reconnected:
                         log.msg('Reconnect in update failed, bailing out.')
                         raise
                     else:
-                        log.msg('Got OperationalError while attempting update: %s.' % str(e))
+                        log.msg('Got InterfaceError while attempting update: %s.' % str(e))
                         log.msg('Attempting reconnect.')
                         self.has_reconnected = True
                         self.pool_proxy.reconnect()
+                except:
+                    conn.rollback()
+                    raise
 
         except Exception, e:
             log.err(e)
@@ -188,6 +136,7 @@ class AggregationUpdater(service.Service):
 
         finally:
             self.updating = False
+            conn.close()
 
 
     def rebuild(self):
