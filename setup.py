b59dd19d608aa4547d86a2a205839d21e1598110
Henrik Thostrup Jensen <htj@ndgf.org>
2010-09-23 13:22:54 +0000

    implement support for relocation using --home

--- setup.py
+++ setup.py
@@ -1,28 +1,52 @@
 import os
 from distutils.core import setup
+from distutils.command.install import install
 from distutils.command.install_data import install_data
 
 from sgas import __version__
 
 
+# nasty global for relocation
+RELOCATE = None
+
+class InstallSGAS(install):
+
+    def finalize_options(self):
+        install.finalize_options(self)
+
+        global RELOCATE ; RELOCATE = self.home
+
+
+
 class InstallSGASData(install_data):
-    # this class is used to filter out data files which should not be overwritten
-    # currently this is sgas.conf and sgas.authz
+    # this class is used for relocating datafiles, and remove existing etc files
+    # so we don't overwrite the configuration of existing sites
 
     def finalize_options(self):
         install_data.finalize_options(self)
 
-        sgas_conf = '/etc/sgas.conf'
-        if self.root is not None:
-            sgas_conf = os.path.join(self.root, sgas_conf[1:])
+        # relocation
+        if RELOCATE:
+            print 'relocating to %s' % RELOCATE
+            for (prefix, files) in reversed(self.data_files):
+                if prefix.startswith('/'):
+                    new_prefix = os.path.join(RELOCATE, prefix[1:])
+                    self.data_files.remove((prefix, files))
+                    self.data_files.append((new_prefix, files))
 
-        if os.path.exists(sgas_conf):
-            print "Skipping installation of etc/sgas.conf and etc/sgas.authz (already exists)"
-            self.data_files.remove( ('/etc/', ['datafiles/etc/sgas.conf']) )
-            self.data_files.remove( ('/etc/', ['datafiles/etc/sgas.authz']) )
+        # check that we don't overwrite /etc files
+        for (prefix, files) in reversed(self.data_files):
+            if prefix.startswith(os.path.join(RELOCATE, 'etc')):
+                for basefile in files:
+                    fn = os.path.join(prefix, os.path.basename(basefile))
+                    if os.path.exists(fn):
+                        print 'Skipping installation of %s (already exists)' % fn
+                        files.remove(basefile)
+            if not files:
+                self.data_files.remove((prefix, []))
 
 
-cmdclasses = {'install_data': InstallSGASData} 
+cmdclasses = {'install': InstallSGAS, 'install_data': InstallSGASData} 
 
 
 setup(name='sgas-luts-service',
