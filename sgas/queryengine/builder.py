a642a35db208225170844405efba4f33fbb2a530
Henrik Thostrup Jensen <htj@ndgf.org>
2010-10-13 14:39:24 +0000

    add parser and query for new query engine

---
+++ sgas/queryengine/builder.py
@@ -0,0 +1,99 @@
+"""
+Query builder SGAS queryengine.
+
+Used for creating SQL query from a set of query arguments.
+
+Author: Henrik Thostrup Jensen <htj@ndgf.org>
+Copyright: Nordic Data Grid Facility (2010)
+"""
+
+import psycopg2.extensions
+
+
+def buildQuery(query_args):
+
+    print "QA", query_args
+
+    user_identities = query_args.get('user_identity')
+    machine_names   = query_args.get('machine_name')
+    vo_name         = query_args.get('vo_name')
+    start_date      = query_args.get('start_date')
+    end_date        = query_args.get('end_date')
+    time_resolution = query_args.get('time_resolution')
+
+    assert time_resolution in ['day', 'week', 'month', 'collapse'], 'Invalid time resolution specified'
+
+    date_extract, date_grouping = _getStartEndDatesAndGrouping(query_args)
+    print "DE", date_extract
+    print "DG", date_grouping
+
+    query_args = [] # RENAME me!
+
+    query = "SELECT machine_name, user_identity, "
+    if vo_name:
+        query += "vo_name, "
+    query += date_extract
+
+    query += "sum(n_jobs), sum(cputime), sum(walltime) "
+    query += "FROM uraggregated "
+    query += "WHERE execution_time > %s AND execution_time < %s "
+    query_args.append(start_date)
+    query_args.append(end_date)
+
+    if user_identities:
+        query += 'AND user_identity IN %s '
+        query_args.append(tuple(user_identities))
+    if machine_names:
+        query += 'AND machine_name IN %s '
+        query_args.append(tuple(machine_names))
+    if vo_name:
+        query += 'AND vo_name = %s '
+        query_args.append(vo_name)
+
+    if query.endswith(','):
+        query = query[:-1]
+
+    query += "GROUP BY "
+    if user_identities:
+        query += 'user_identity,'
+    if machine_names:
+        query += 'machine_name,'
+    if vo_name:
+        query += 'vo_name,'
+
+    query += date_grouping
+
+    if query.endswith(','):
+        query = query[:-1]
+
+#    query_args += [start_date, end_date, machine_names[0], user_identities[0] ]
+
+    print "Q", query
+    print "A", query_args
+    return query, query_args
+
+
+
+def _getStartEndDatesAndGrouping(query_args):
+
+    time_resolution = query_args.get('time_resolution')
+
+    if time_resolution == 'day':
+        dates = 'execution_time, execution_time, '
+        group = 'execution_time,'
+
+    elif time_resolution == 'week':
+        dates = ''
+        group = "date_part('year', current_date) || '-' || date_part('week', current_date),"
+
+    elif time_resolution == 'montly':
+        dates = ' .. '
+        group = "date_part('year', current_date) || '-' || date_part('month', current_date),"
+
+    elif time_resolution == 'collapse':
+        dates = '%s, %s, ' % (query_args.get('start_date'), query_args.get('end_date'))
+        group = ''
+
+    return dates, group
+
+
--- test/ursampledata.py
+++ test/ursampledata.py
@@ -6,7 +6,7 @@ Some sample usage records.
 UR1_ID = 'gsiftp://example.org/jobs/1'
 UR1 = """<?xml version="1.0" encoding="UTF-8" ?>
 <ur:JobUsageRecord xmlns:ur="http://schema.ogf.org/urf/2003/09/urf">
-    <ur:RecordIdentity ur:createTime="2009-07-07T09:06:52Z" ur:recordId="gsiftp://example.org/jobs/1" />
+    <ur:RecordIdentity ur:createTime="2009-08-09T09:06:52Z" ur:recordId="gsiftp://example.org/jobs/1" />
     <ur:JobIdentity>
         <ur:GlobalJobId>gsiftp://example.org/jobs/1</ur:GlobalJobId>
     </ur:JobIdentity>
@@ -15,9 +15,9 @@ UR1 = """<?xml version="1.0" encoding="UTF-8" ?>
     </ur:UserIdentity>
     <ur:JobName>test job 1</ur:JobName>
     <ur:MachineName>benedict.grid.aau.dk</ur:MachineName>
-    <ur:StartTime>2009-07-07T09:06:37Z</ur:StartTime>
-    <ur:EndTime>2009-07-07T09:06:52Z</ur:EndTime>
-    <ur:WallDuration>PT1S</ur:WallDuration>
+    <ur:StartTime>2009-08-07T09:06:37Z</ur:StartTime>
+    <ur:EndTime>2009-08-07T09:06:52Z</ur:EndTime>
+    <ur:WallDuration>PT234S</ur:WallDuration>
 </ur:JobUsageRecord>
 """
 
@@ -25,7 +25,7 @@ UR1 = """<?xml version="1.0" encoding="UTF-8" ?>
 UR2_ID = 'gsiftp://example.org/jobs/2'
 UR2 = """<?xml version="1.0" encoding="UTF-8" ?>
 <ur:JobUsageRecord xmlns:ur="http://schema.ogf.org/urf/2003/09/urf">
-    <ur:RecordIdentity ur:createTime="2009-07-07T09:06:52Z" ur:recordId="gsiftp://example.org/jobs/2" />
+    <ur:RecordIdentity ur:createTime="2009-09-10T09:06:52Z" ur:recordId="gsiftp://example.org/jobs/2" />
     <ur:JobIdentity>
         <ur:GlobalJobId>gsiftp://example.org/jobs/2</ur:GlobalJobId>
     </ur:JobIdentity>
@@ -34,9 +34,9 @@ UR2 = """<?xml version="1.0" encoding="UTF-8" ?>
     </ur:UserIdentity>
     <ur:JobName>test job 2</ur:JobName>
     <ur:MachineName>benedict.grid.aau.dk</ur:MachineName>
-    <ur:StartTime>2009-07-08T09:06:37Z</ur:StartTime>
-    <ur:EndTime>2009-07-08T09:06:52Z</ur:EndTime>
-    <ur:WallDuration>PT2S</ur:WallDuration>
+    <ur:StartTime>2009-09-10T09:06:37Z</ur:StartTime>
+    <ur:EndTime>2009-09-10T09:06:52Z</ur:EndTime>
+    <ur:WallDuration>PT345S</ur:WallDuration>
 </ur:JobUsageRecord>
 """
 
@@ -45,7 +45,7 @@ CUR_IDS = [ "gsiftp://example.org/jobs/3", "gsiftp://example.org/jobs/4" ]
 CUR = """<?xml version="1.0" encoding="UTF-8" ?>
 <ur:UsageRecords xmlns:ur="http://schema.ogf.org/urf/2003/09/urf">
   <ur:JobUsageRecord>
-    <ur:RecordIdentity ur:createTime="2009-07-07T09:06:52Z" ur:recordId="gsiftp://example.org/jobs/3" />
+    <ur:RecordIdentity ur:createTime="2009-10-11T09:06:52Z" ur:recordId="gsiftp://example.org/jobs/3" />
     <ur:JobIdentity>
         <ur:GlobalJobId>gsiftp://example.org/jobs/3</ur:GlobalJobId>
     </ur:JobIdentity>
@@ -54,12 +54,12 @@ CUR = """<?xml version="1.0" encoding="UTF-8" ?>
     </ur:UserIdentity>
     <ur:JobName>test job 3</ur:JobName>
     <ur:MachineName>benedict.grid.aau.dk</ur:MachineName>
-    <ur:StartTime>2009-07-08T09:06:37Z</ur:StartTime>
-    <ur:EndTime>2009-07-08T09:06:52Z</ur:EndTime>
-    <ur:WallDuration>PT3S</ur:WallDuration>
+    <ur:StartTime>2009-10-11T09:06:37Z</ur:StartTime>
+    <ur:EndTime>2009-10-11T09:06:52Z</ur:EndTime>
+    <ur:WallDuration>PT456S</ur:WallDuration>
   </ur:JobUsageRecord>
   <ur:JobUsageRecord>
-    <ur:RecordIdentity ur:createTime="2009-07-07T09:06:52Z" ur:recordId="gsiftp://example.org/jobs/4" />
+    <ur:RecordIdentity ur:createTime="2009-12-13T09:06:52Z" ur:recordId="gsiftp://example.org/jobs/4" />
     <ur:JobIdentity>
         <ur:GlobalJobId>gsiftp://example.org/jobs/4</ur:GlobalJobId>
     </ur:JobIdentity>
@@ -68,9 +68,9 @@ CUR = """<?xml version="1.0" encoding="UTF-8" ?>
     </ur:UserIdentity>
     <ur:JobName>test job 4</ur:JobName>
     <ur:MachineName>fyrgrid.grid.aau.dk</ur:MachineName>
-    <ur:StartTime>2009-07-08T09:06:37Z</ur:StartTime>
-    <ur:EndTime>2009-07-08T09:06:52Z</ur:EndTime>
-    <ur:WallDuration>PT4S</ur:WallDuration>
+    <ur:StartTime>2009-12-13T09:06:37Z</ur:StartTime>
+    <ur:EndTime>2009-12-13T09:06:52Z</ur:EndTime>
+    <ur:WallDuration>PT678S</ur:WallDuration>
   </ur:JobUsageRecord>
 </ur:UsageRecords>
 """
