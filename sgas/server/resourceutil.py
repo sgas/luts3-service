60015bf152a9876b34cd2705b73ac726831e2905
Henrik Thostrup Jensen <htj@ndgf.org>
2010-11-03 12:47:46 +0000

    move getSubject from authz engine to seperate file in server module

---
+++ sgas/server/resourceutil.py
@@ -0,0 +1,25 @@
+"""
+HTTP server resource utilities.
+"""
+
+
+
+def getSubject(request):
+    """
+    Utility method for extracting the subject name from a twisted.web.http.Request
+    """
+    if request.isSecure():
+        x509 = request.transport.getPeerCertificate()
+        if x509:
+            #print x509.get_subject().get_components()
+            subject = '/' + '/'.join([ '='.join(c) for c in x509.get_subject().get_components() ])
+            return subject
+
+    # identity forwarded by reverse proxy
+    if request.getClientIP() in ('127.0.0.1', '::1') and 'x-ssl-subject' in request.received_headers:
+        return request.received_headers.get('x-ssl-subject')
+
+    # request wasn't secure or no certificate was presented
+    return None
+
+
