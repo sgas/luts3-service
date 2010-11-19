4a37dcbcc4ba2fac68ae7d6bfd8e5a07269e9fb4
Henrik Thostrup Jensen <htj@ndgf.org>
2010-11-19 12:50:37 +0000

    always include project name in query results (have confused all users so far)

--- sgas/queryengine/builder.py
+++ sgas/queryengine/builder.py
@@ -24,9 +24,7 @@ def buildQuery(query_args):
 
     query_args = [] # RENAME me!
 
-    query = "SELECT machine_name, user_identity, "
-    if vo_name:
-        query += "vo_name, "
+    query = "SELECT machine_name, user_identity, vo_name, "
     query += date_extract
 
     query += "sum(n_jobs), sum(cputime), sum(walltime) "
@@ -48,9 +46,7 @@ def buildQuery(query_args):
     if query.endswith(','):
         query = query[:-1]
 
-    query += "GROUP BY machine_name, user_identity,"
-    if vo_name:
-        query += 'vo_name,'
+    query += "GROUP BY machine_name, user_identity, vo_name,"
 
     query += date_grouping
 
