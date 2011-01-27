2a1603d0a9f442efe8f3e1616af9e39800b8b0a3
Henrik Thostrup Jensen <htj@ndgf.org>
2011-01-27 22:09:22 +0000

    add manifest for keeping track of service stats

---
+++ sgas/server/manifest.py
@@ -0,0 +1,56 @@
+"""
+Service manifest, i.e., various information about state of the server and
+what has happened.
+
+Author: Henrik Thostrup Jensen <htj@ndgf.org>
+Copyright: Nordic Data Grid Facility (2011)
+"""
+
+
+class NoSuchPropertyError(KeyError):
+    """
+    Raised when trying to access a property that does not exist.
+    """
+
+
+class PropertyTypeError(TypeError):
+    """
+    Raised when trying to handle a property value in a way that does not
+    match with its type.
+    """
+
+
+class Manifest:
+
+    def __init__(self):
+
+        self.properties = {}
+
+
+    def setProperty(self, property_, value):
+
+        self.properties[property_] = value
+
+
+    def getProperty(self, property_, value):
+
+        try:
+            return self.properties[property_]
+        except KeyError, e:
+            raise NoSuchPropertyError(e)
+
+
+    def incrementProperty(self, property_, count):
+
+        assert type(count) is int, 'Count parameter must be an integer'
+
+        if not property_ in self.properties:
+            self.properties[property_] = count
+        else:
+            self.properties[property_] += count
+
+
+    def getAllProperties(self):
+
+        return self.properties.copy()
+
