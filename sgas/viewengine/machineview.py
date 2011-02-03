3c941c52851ebd3eaec951c0bf70a826541be93a
Henrik Thostrup Jensen <htj@ndgf.org>
2011-02-03 12:46:45 +0000

    add project stats to machine view page

--- sgas/viewengine/machineview.py
+++ sgas/viewengine/machineview.py
@@ -25,13 +25,22 @@ FROM (SELECT current_date - s as dates FROM generate_series(0,8) as s) as d
 GROUP BY d.dates ORDER BY d.dates;
 """
 
-QUERY_TOP10_USERS = """
+QUERY_TOP10_PROJECTS = """
+SELECT vo_name, sum(walltime)::integer as walltime, sum(n_jobs)
+FROM uraggregated
+WHERE execution_time > current_date - interval '1 month' AND machine_name = %s
+GROUP BY vo_name
+ORDER BY walltime DESC
+LIMIT 20;
+"""
+
+QUERY_TOP20_USERS = """
 SELECT user_identity, sum(walltime)::integer as walltime, sum(n_jobs)
 FROM uraggregated
 WHERE execution_time > current_date - interval '1 month' AND machine_name = %s
 GROUP BY user_identity
 ORDER BY walltime DESC
-LIMIT 10;
+LIMIT 20;
 """
 
 
@@ -115,7 +124,7 @@ class MachineView(baseview.BaseView):
     def retrieveMachineInfo(self):
 
         defs = []
-        for query in [ QUERY_EXECUTED_JOBS_PER_DAY, QUERY_TOP10_USERS ]:
+        for query in [ QUERY_EXECUTED_JOBS_PER_DAY, QUERY_TOP10_PROJECTS, QUERY_TOP20_USERS ]:
             d = self.urdb.query(query, (self.machine_name,))
             defs.append(d)
 
@@ -126,20 +135,28 @@ class MachineView(baseview.BaseView):
     def renderMachineView(self, results, request):
 
         jobs_per_day = results[0][1]
-        top_users = results[1][1]
+        top_projects = results[1][1]
+        top_users    = results[2][1]
 
         e_batches = [ e[0] for e in jobs_per_day ] # dates
         e_groups  = [ 'jobs' ]
         e_matrix  = dict( [ ((e[0], e_groups[0]), e[1]) for e in jobs_per_day ] )
         executed_table = htmltable.createHTMLTable(e_matrix, e_batches, e_groups)
 
+        p_batches = [ 'Walltime hours', 'Number of jobs' ]
+        p_groups = [ e[0] for e in top_projects ] # user list
+        p_dict_elements = []
+        p_dict_elements += [ ((p_batches[0], p[0]), p[1]) for p in top_projects ]
+        p_dict_elements += [ ((p_batches[1], p[0]), p[2]) for p in top_projects ]
+        p_matrix = dict(p_dict_elements)
+        project_table = htmltable.createHTMLTable(p_matrix, p_batches, p_groups)
+
         u_batches = [ 'Walltime hours', 'Number of jobs' ]
         u_groups = [ e[0] for e in top_users ] # user list
-        dict_elements = []
-        dict_elements += [ ((u_batches[0], u[0]), u[1]) for u in top_users ]
-        dict_elements += [ ((u_batches[1], u[0]), u[2]) for u in top_users ]
-
-        u_matrix = dict(dict_elements)
+        u_dict_elements = []
+        u_dict_elements += [ ((u_batches[0], u[0]), u[1]) for u in top_users ]
+        u_dict_elements += [ ((u_batches[1], u[0]), u[2]) for u in top_users ]
+        u_matrix = dict(u_dict_elements)
         user_table = htmltable.createHTMLTable(u_matrix, u_batches, u_groups)
 
         title = 'Machine view for %s' % self.machine_name
@@ -152,7 +169,11 @@ class MachineView(baseview.BaseView):
         request.write(executed_table)
         request.write('<p> &nbsp; <p>\n')
 
-        request.write('<h5>Top users for the last month</h5>\n')
+        request.write('<h5>Top 10 projects in the last month</h5>\n')
+        request.write(project_table)
+        request.write('<p> &nbsp; <p>\n')
+
+        request.write('<h5>Top 20 users for the last month</h5>\n')
         request.write(user_table)
         request.write('<p>\n')
 
