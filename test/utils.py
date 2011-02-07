d2bbbe284ef030f32bbb5b74e1e544aaf30a2006
Henrik Thostrup Jensen <htj@ndgf.org>
2011-02-07 10:37:48 +0000

    have log message for fake authz check used in testing (so it can be seen which checks are made)

--- test/utils.py
+++ test/utils.py
@@ -2,6 +2,8 @@
 Various testing utilities.
 """
 
+from twisted.python import log
+
 
 class FakeAuthorizer:
     # silly authorizer for testing setups without authz part
@@ -10,5 +12,6 @@ class FakeAuthorizer:
         return True
 
     def isAllowed(self, subject, action, context=None):
+        log.msg('Authz check (fake): %s, %s, %s' % (subject, action, context))
         return True
 
