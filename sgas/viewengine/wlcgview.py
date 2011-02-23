00fea5ae268c05663dee227a2af2c947722cc165
Henrik Thostrup Jensen <htj@ndgf.org>
2011-02-23 12:52:04 +0000

    add split functionality and reduce some of the hardcode for the view listing

--- sgas/viewengine/wlcgview.py
+++ sgas/viewengine/wlcgview.py
@@ -5,6 +5,7 @@ Author: Henrik Thostrup Jensen <htj@ndgf.org>
 Copyright: Nordic Data Grid Facility (2011)
 """
 
+import time
 import json
 
 from wlcgsgas import query as wlcgquery, dataprocess
@@ -20,17 +21,25 @@ from sgas.viewengine import html, htmltable, dateform, baseview
 class WLCGView(baseview.BaseView):
 
 
+    def __init__(self, urdb, authorizer, manifest):
+
+        baseview.BaseView.__init__(self, urdb, authorizer, manifest)
+
+        self.subview = {
+            'machine'   : ('WLCG machine view', WLCGMachineView(self.urdb, self.authorizer, self.manifest, 'machine')),
+            'vo'        : ('WLCG VO view',      WLCGVOView(self.urdb, self.authorizer, self.manifest, 'vo')),
+            'user'      : ('WLCG User view',    WLCGUserView(self.urdb, self.authorizer, self.manifest, 'user')),
+            'tier'      : ('WLCG tier view',    WLCGTierView(self.urdb, self.authorizer, self.manifest, 'tier')),
+            'fulltier'  : ('WLCG full tier view', WLCGFullTierView(self.urdb, self.authorizer, self.manifest, 'fulltier')),
+            'tiersplit' : ('WLCG tier-machine split view', WLCGTierMachineSplitView(self.urdb, self.authorizer, self.manifest, 'tiersplit'))
+        }
+
     def getChild(self, path, request):
-        if path == 'machine':
-            return WLCGMachineView(self.urdb, self.authorizer, self.manifest, path)
-        elif path == 'vo':
-            return WLCGVOView(self.urdb, self.authorizer, self.manifest, path)
-        elif path == 'tier':
-            return WLCGTierView(self.urdb, self.authorizer, self.manifest, path)
-        elif path == 'user':
-            return WLCGUserView(self.urdb, self.authorizer, self.manifest, path)
-        # no such resource
-        return baseview.BaseView.getChild(self, path, request)
+        if path in self.subview:
+            return self.subview[path][1]
+        else:
+            # no such resource
+            return baseview.BaseView.getChild(self, path, request)
 
 
     def render_GET(self, request):
@@ -45,16 +54,10 @@ class WLCGView(baseview.BaseView):
         # access allowed
         request.write(html.HTML_VIEWBASE_HEADER % {'title': 'WLCG Views'})
         request.write( html.createTitle('WLCG Views') )
-        request.write('<div><a href=wlcg/machine>WLCG machine view</a></div>\n')
-        request.write( html.P )
-        request.write('<div><a href=wlcg/vo>WLCG VO View</a></div>\n')
-        request.write( html.P )
-        request.write('<div><a href=wlcg/tier>WLCG tier view</a></div>\n')
-        request.write( html.P )
-        request.write('<div><a href=wlcg/user>WLCG user view</a></div>\n')
-        request.write( html.P )
+        for view_url, ( description, _ ) in self.subview.items():
+            request.write('<div><a href=wlcg/%s>%s</a></div>\n' % (view_url, description))
+            request.write( html.P )
         request.write(html.HTML_VIEWBASE_FOOTER)
-
         request.finish()
         return server.NOT_DONE_YET
 
@@ -64,6 +67,7 @@ class WLCGBaseView(baseview.BaseView):
 
     collapse = []
     columns = []
+    split = None
     tier_based = False
 
     def __init__(self, urdb, authorizer, mfst, path):
@@ -86,8 +90,9 @@ class WLCGBaseView(baseview.BaseView):
 
         # access allowed
         start_date, end_date = dateform.parseStartEndDates(request)
+        t_query_start = time.time()
         d = self.retrieveWLCGData(start_date, end_date)
-        d.addCallbacks(self.renderWLCGViewPage, self.renderErrorPage, callbackArgs=(request, start_date, end_date), errbackArgs=(request,))
+        d.addCallbacks(self.renderWLCGViewPage, self.renderErrorPage, callbackArgs=(request, start_date, end_date, t_query_start), errbackArgs=(request,))
         return server.NOT_DONE_YET
 
 
@@ -97,7 +102,9 @@ class WLCGBaseView(baseview.BaseView):
         return d
 
 
-    def renderWLCGViewPage(self, wlcg_data, request, start_date, end_date):
+    def renderWLCGViewPage(self, wlcg_data, request, start_date, end_date, t_query_start):
+
+        t_query = time.time() - t_query_start
 
         def formatValue(v):
             if type(v) is float:
@@ -109,28 +116,53 @@ class WLCGBaseView(baseview.BaseView):
 
         # massage data
         #print "L1", len(wlcg_data)
+        t_dataprocess_start = time.time()
         wlcg_records = dataprocess.rowsToDicts(wlcg_data)
         wlcg_records = dataprocess.addMissingScaleValues(wlcg_records)
         wlcg_records = dataprocess.collapseFields(wlcg_records, self.collapse)
         if self.tier_based:
             wlcg_records = dataprocess.tierMergeSplit(wlcg_records, self.tier_mapping, self.tier_shares, self.default_tier)
-        wlcg_records = dataprocess.addEffiencyProperty(wlcg_records)
-        wlcg_records = dataprocess.addEquivalentProperties(wlcg_records, days)
-        sk = lambda key : dataprocess.sortKey(key, field_order=self.columns)
-        wlcg_records = sorted(wlcg_records, key=sk)
-        #print "L2", len(wlcg_records)
+            if self.split != dataprocess.TIER:
+                wlcg_records = dataprocess.collapseFields(wlcg_records, ( dataprocess.HOST, ) )
         # information on ops vo does not add any value
         wlcg_records = [ rec for rec in wlcg_records if rec[dataprocess.VO_NAME] != 'ops' ]
-        #print "L3", len(wlcg_records)
+        wlcg_records = dataprocess.addEffiencyProperty(wlcg_records)
+        wlcg_records = dataprocess.addEquivalentProperties(wlcg_records, days)
 
-        row_names = range(0, len(wlcg_records))
+        if self.split is None:
+            sk = lambda key : dataprocess.sortKey(key, field_order=self.columns)
+            wlcg_records = sorted(wlcg_records, key=sk)
+        else:
+            split_records = dataprocess.splitRecords(wlcg_records, dataprocess.TIER)
 
-        elements = []
-        for rn in row_names:
-            for c in self.columns:
-                elements.append( ((c, rn), formatValue(wlcg_records[rn][c])) )
-        matrix = dict(elements)
-        table = htmltable.createHTMLTable(matrix, self.columns, row_names, skip_base_column=True)
+        #print "L2", len(wlcg_records)
+        t_dataprocess = time.time() - t_dataprocess_start
+
+
+        if self.split is None:
+            row_names = range(0, len(wlcg_records))
+            elements = []
+            for rn in row_names:
+                for c in self.columns:
+                    elements.append( ((c, rn), formatValue(wlcg_records[rn][c])) )
+            matrix = dict(elements)
+            table_content = htmltable.createHTMLTable(matrix, self.columns, row_names, skip_base_column=True)
+
+        else:
+            table_content = ''
+            for split_attr, records in split_records.items():
+                sk = lambda key : dataprocess.sortKey(key, field_order=self.columns)
+                records = sorted(records, key=sk)
+                row_names = range(0, len(records))
+                elements = [] 
+                for rn in row_names:
+                    for c in self.columns:
+                        elements.append( ((c, rn), formatValue(records[rn][c])) )
+                matrix = dict(elements)
+                table = htmltable.createHTMLTable(matrix, self.columns, row_names, skip_base_column=True)
+                table_content += html.createParagraph(split_attr)
+                table_content += table
+                table_content += html.SECTION_BREAK
 
         start_date_option = request.args.get('startdate', [''])[0]
         end_date_option   = request.args.get('enddate', [''])[0]
@@ -144,7 +176,10 @@ class WLCGBaseView(baseview.BaseView):
         request.write( html.createParagraph(selector_form) )
         request.write( html.SECTION_BREAK )
         request.write( html.createParagraph(range_text) )
-        request.write( table )
+        request.write( table_content )
+        request.write( html.SECTION_BREAK )
+        request.write( html.createParagraph('Query time: %s' % round(t_query, 2)) )
+        request.write( html.createParagraph('Data process time: %s' % round(t_dataprocess, 2)) )
         request.write( html.HTML_VIEWBASE_FOOTER )
 
         request.finish()
@@ -167,6 +202,24 @@ class WLCGTierView(WLCGBaseView):
                 dataprocess.N_JOBS, dataprocess.KSI2K_WALL_TIME, dataprocess.KSI2K_WALL_EQUIVALENTS, dataprocess.EFFICIENCY ]
     tier_based = True
 
+
+
+class WLCGFullTierView(WLCGBaseView):
+
+    collapse = ( dataprocess.YEAR, dataprocess.MONTH )
+    columns = [ dataprocess.TIER, dataprocess.VO_NAME, dataprocess.VO_GROUP, dataprocess.VO_ROLE, dataprocess.USER,
+                dataprocess.N_JOBS, dataprocess.KSI2K_WALL_TIME, dataprocess.KSI2K_WALL_EQUIVALENTS, dataprocess.EFFICIENCY ]
+    tier_based = True
+
+
+
+class WLCGTierMachineSplitView(WLCGBaseView):
+
+    collapse = ( dataprocess.YEAR, dataprocess.MONTH, dataprocess.VO_GROUP, dataprocess.USER )
+    columns = [ dataprocess.HOST, dataprocess.VO_NAME, dataprocess.VO_ROLE,
+                dataprocess.N_JOBS, dataprocess.KSI2K_WALL_TIME, dataprocess.KSI2K_WALL_EQUIVALENTS, dataprocess.EFFICIENCY ]
+    tier_based = True
+    split = dataprocess.TIER
 
 
 class WLCGMachineView(WLCGBaseView):
