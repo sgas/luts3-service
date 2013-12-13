c28bd3cc028968c792ad541f886dbe37cf1df7e5
Magnus Jonsson <magnus@hpc2n.umu.se>
2013-12-13 09:18:56 +0100

    Fixes #1 Problem with long running, large jobs

--- datafiles/share/postgresql/sgas-postgres-3.6.2-3.6.3-upgrade.sql
+++ datafiles/share/postgresql/sgas-postgres-3.6.2-3.6.3-upgrade.sql
@@ -1,5 +1,70 @@
--- SGAS PostgreSQL functions
+-- logic for upgrading the SGAS PostgreSQL schema from version 3.6.2 to 3.6.3
+-- SGAS should be stopped when performing this upgrade
 
+-- Drop View (needed by alter table)
+DROP VIEW usagerecords;
+
+-- Alter table
+ALTER TABLE usagedata ALTER cpu_duration TYPE bigint;
+
+-- Create view
+CREATE VIEW usagerecords AS
+SELECT
+    record_id,
+    create_time,
+    globalusername.global_user_name,
+    voinformation.vo_type,
+    voinformation.vo_issuer,
+    voinformation.vo_name,
+    voinformation.vo_attributes,
+    machinename.machine_name,
+    global_job_id,
+    local_job_id,
+    localuser.local_user,
+    job_name,
+    charge,
+    jobstatus.status,
+    jobqueue.queue,
+    host.host,
+    node_count,
+    processors,
+    projectname.project_name,
+    submithost.submit_host,
+    start_time,
+    end_time,
+    submit_time,
+    cpu_duration,
+    wall_duration,
+    cpu_duration  * (SELECT scale_factor FROM hostscalefactors WHERE machine_name = machinename.machine_name) AS cpu_duration_scaled,
+    wall_duration * (SELECT scale_factor FROM hostscalefactors WHERE machine_name = machinename.machine_name) AS wall_duration_scaled,
+    user_time,
+    kernel_time,
+    major_page_faults,
+    ARRAY(SELECT runtimeenvironment.runtime_environment
+          FROM runtimeenvironment, runtimeenvironment_usagedata
+          WHERE usagedata.id = runtimeenvironment_usagedata.usagedata_id AND
+                runtimeenvironment_usagedata.runtimeenvironments_id = runtimeenvironment.id)
+    AS runtime_environments,
+    exit_code,
+    inserthost.insert_host,
+    insertidentity.insert_identity,
+    insert_time
+FROM
+    usagedata
+LEFT OUTER JOIN globalusername  ON (usagedata.global_user_name_id = globalusername.id)
+LEFT OUTER JOIN voinformation   ON (usagedata.vo_information_id   = voinformation.id)
+LEFT OUTER JOIN localuser       ON (usagedata.local_user_id       = localuser.id)
+LEFT OUTER JOIN machinename     ON (usagedata.machine_name_id     = machinename.id)
+LEFT OUTER JOIN jobqueue        ON (usagedata.queue_id            = jobqueue.id)
+LEFT OUTER JOIN host            ON (usagedata.host_id             = host.id)
+LEFT OUTER JOIN jobstatus       ON (usagedata.status_id           = jobstatus.id)
+LEFT OUTER JOIN projectname     ON (usagedata.project_name_id     = projectname.id)
+LEFT OUTER JOIN submithost      ON (usagedata.submit_host_id      = submithost.id)
+LEFT OUTER JOIN inserthost      ON (usagedata.insert_host_id      = inserthost.id)
+LEFT OUTER JOIN insertidentity  ON (usagedata.insert_identity_id  = insertidentity.id)
+;
+
+-- Update functions
 CREATE OR REPLACE FUNCTION urcreate (
     in_record_id               varchar,
     in_create_time             timestamp,
@@ -342,286 +407,3 @@ BEGIN
 END;
 $recordid_rowid$
 LANGUAGE plpgsql;
-
-
-
-CREATE OR REPLACE FUNCTION update_uraggregate ( )
-RETURNS varchar[] AS $insertdate_machinename$
-
-DECLARE
-    q_insert_date       date;
-    q_machine_name_id   integer;
-    result              varchar[];
-BEGIN
-    -- the transaction isolation level for this function should be set to serializable
-    -- unfortunately it is not possible to set in the function as a function always
-    -- start a transaction, at which time it is to late to set it
-    -- therefore we trust the caller of the function to set it for us
-
-    -- get data for what to update
-    SELECT insert_time, machine_name_id INTO q_insert_date, q_machine_name_id
-        FROM uraggregated_update ORDER BY insert_time LIMIT 1;
-    IF NOT FOUND THEN
-        -- nothing to update
-        RETURN result;
-    END IF;
-
-    -- delete aggregation update row
-    DELETE FROM uraggregated_update WHERE insert_time = q_insert_date AND machine_name_id = q_machine_name_id;
-    -- delete existing aggregated rows that will be updated
-    DELETE FROM uraggregated_data WHERE insert_time = q_insert_date AND machine_name_id = q_machine_name_id;
-
-    INSERT INTO uraggregated_data
-        (execution_time, insert_time, machine_name_id, queue_id,
-         global_user_name_id, local_user_id, vo_information_id, project_name_id,
-         runtime_environments_id, status_id, insert_host_id, n_jobs, cputime, walltime, generate_time)
-    SELECT
-        COALESCE(end_time::DATE, create_time::DATE)                             AS s_execute_time,
-        insert_time::DATE                                                       AS s_insert_time,
-        machine_name_id                                                         AS s_machine_name_id,
-        queue_id                                                                AS s_queue_id,
-        global_user_name_id                                                     AS s_global_user_name_id,
-        CASE WHEN global_user_name_id IS NULL THEN local_user_id ELSE NULL END  AS s_local_user_id,
-        vo_information_id                                                       AS s_vo_information_id,
-        CASE WHEN vo_information_id IS NULL THEN project_name_id ELSE NULL END  AS s_project_name_id,
-        ARRAY(SELECT runtimeenvironment_usagedata.runtimeenvironments_id
-              FROM runtimeenvironment_usagedata
-              WHERE usagedata.id = runtimeenvironment_usagedata.usagedata_id)   AS s_runtime_environments,
-        status_id                                                               AS s_status_id,
-        insert_host_id                                                          AS s_insert_host_id,
-        count(*)                                                                AS s_n_jobs,
-        SUM(COALESCE(cpu_duration::bigint,0))                                   AS s_cputime,
-        SUM(COALESCE(wall_duration::bigint,0) * COALESCE(processors,1))         AS s_walltime,
-        now()                                                                   AS s_generate_time
-    FROM
-        usagedata
-    WHERE
-        insert_time::date = q_insert_date AND machine_name_id = q_machine_name_id
-    GROUP BY
-        s_execute_time, s_insert_time, s_machine_name_id, s_queue_id,
-        s_global_user_name_id, s_local_user_id, s_vo_information_id, s_project_name_id,
-        s_runtime_environments, s_status_id, s_insert_host_id;
-
-    result[0] = q_insert_date::varchar;
-    result[1] = q_machine_name_id;
-    RETURN result;
-
-END;
-$insertdate_machinename$
-LANGUAGE plpgsql;
-
-
-
-CREATE OR REPLACE FUNCTION srcreate (
-    in_record_id            varchar,
-    in_create_time          timestamp,
-    in_storage_system       varchar,
-    in_storage_share        varchar,
-    in_storage_media        varchar,
-    in_storage_class        varchar,
-    in_file_count           integer,
-    in_directory_path       varchar,
-    in_local_user           varchar,
-    in_local_group          varchar,
-    in_user_identity        varchar,
-    in_group_identity       varchar,
-    in_group_attribute     varchar[][],
-    in_measure_time         timestamp,
-    in_valid_duration       integer,
-    in_resource_capacity_used   bigint,
-    in_logical_capacity_used    bigint,
-    in_insert_host          varchar,
-    in_insert_identity      varchar,
-    in_insert_time          timestamp
-)
-RETURNS
-    varchar[] as $recordid_rowid$
-DECLARE
-    storage_system_key      integer;
-    storage_share_key       integer;
-    storage_media_key       integer;
-    storage_class_key       integer;
-    directory_path_key      integer;
-    local_user_key          integer;
-    local_group_key         integer;
-    user_identity_key       integer;
-    group_identity_key      integer;
-    insert_host_key         integer;
-    insert_identity_key     integer;
-
-    sr_key                  integer;
-    result                  varchar[];
-BEGIN
-    -- first check if the record already exists
-    SELECT storagedata.id INTO sr_key FROM storagedata WHERE record_id = in_record_id;
-    IF FOUND THEN
-        -- just return the entry from the already existing record
-        result[0] = in_record_id;
-        result[1] = sr_key;
-        RETURN result;
-    END IF;
-
-    -- storage system
-    IF in_storage_system IS NULL THEN
-        RAISE EXCEPTION 'Storage system parameter must be non-null.';
-    ELSE
-        SELECT INTO storage_system_key id FROM storagesystem WHERE storage_system = in_storage_system;
-        IF NOT FOUND THEN
-            INSERT INTO storagesystem (storage_system) VALUES (in_storage_system) RETURNING id INTO storage_system_key;
-        END IF;
-    END IF;
-
-    -- storage share
-    IF in_storage_share IS NULL THEN
-        storage_share_key = NULL;
-    ELSE
-        SELECT INTO storage_share_key id FROM storageshare WHERE storage_share = in_storage_share;
-        IF NOT FOUND THEN
-            INSERT INTO storageshare (storage_share) VALUES (in_storage_share) RETURNING id INTO storage_share_key;
-        END IF;
-    END IF;
-
-    -- storage media
-    IF in_storage_media IS NULL THEN
-        storage_media_key = NULL;
-    ELSE
-        SELECT INTO storage_media_key id FROM storagemedia WHERE storage_media = in_storage_media;
-        IF NOT FOUND THEN
-            INSERT INTO storagemedia (storage_media) VALUES (in_storage_media) RETURNING id INTO storage_media_key;
-        END IF;
-    END IF;
-
-    -- storage class
-    IF in_storage_class IS NULL THEN
-        storage_class_key = NULL;
-    ELSE
-        SELECT INTO storage_class_key id FROM storageclass WHERE storage_class = in_storage_class;
-        IF NOT FOUND THEN
-            INSERT INTO storageclass (storage_class) VALUES (in_storage_class) RETURNING id INTO storage_class_key;
-        END IF;
-    END IF;
-
-    -- directory path
-    IF in_directory_path IS NULL THEN
-        directory_path_key = NULL;
-    ELSE
-        SELECT INTO directory_path_key id FROM directorypath WHERE directory_path = in_directory_path;
-        IF NOT FOUND THEN
-            INSERT INTO directorypath (directory_path) VALUES (in_directory_path) RETURNING id INTO directory_path_key;
-        END IF;
-    END IF;
-
-    -- local user
-    IF in_local_user IS NULL THEN
-        local_user_key = NULL;
-    ELSE
-        SELECT INTO local_user_key id FROM localuser WHERE local_user = in_local_user;
-        IF NOT FOUND THEN
-            INSERT INTO localuser (local_user) VALUES (in_local_user) RETURNING id INTO local_user_key;
-        END IF;
-    END IF;
-
-    -- local group
-    IF in_local_group IS NULL THEN
-        local_group_key = NULL;
-    ELSE
-        SELECT INTO local_group_key id FROM localgroup WHERE local_group = in_local_group;
-        IF NOT FOUND THEN
-            INSERT INTO localgroup (local_group) VALUES (in_local_group) RETURNING id INTO local_group_key;
-        END IF;
-    END IF;
-
-    -- user identity
-    IF in_user_identity IS NULL THEN
-        user_identity_key = NULL;
-    ELSE
-        SELECT INTO user_identity_key id FROM useridentity WHERE user_identity = in_user_identity;
-        IF NOT FOUND THEN
-            INSERT INTO useridentity (user_identity) VALUES (in_user_identity) RETURNING id INTO user_identity_key;
-        END IF;
-    END IF;
-
-    -- group identity
-    IF in_group_identity IS NULL THEN
-        group_identity_key = NULL;
-    ELSE
-        SELECT INTO group_identity_key id FROM groupidentity
-               WHERE group_identity   IS NOT DISTINCT FROM in_group_identity AND
-                     group_attribute  IS NOT DISTINCT FROM in_group_attribute;
-        IF NOT FOUND THEN
-            INSERT INTO groupidentity (group_identity, group_attribute) VALUES (in_group_identity, in_group_attribute) RETURNING id INTO group_identity_key;
-        END IF;
-    END IF;
-
-    -- insert host
-    IF in_insert_host IS NULL THEN
-        insert_host_key = NULL;
-    ELSE
-        SELECT INTO insert_host_key id FROM inserthost WHERE insert_host = in_insert_host;
-        IF NOT FOUND THEN
-            INSERT INTO inserthost (insert_host) VALUES (in_insert_host) RETURNING id INTO insert_host_key;
-        END IF;
-    END IF;
-
-    -- insert identity
-    IF in_insert_identity IS NULL THEN
-        insert_identity_key = NULL;
-    ELSE
-        SELECT INTO insert_identity_key id FROM insertidentity WHERE insert_identity = in_insert_identity;
-        IF NOT FOUND THEN
-            INSERT INTO insertidentity (insert_identity) VALUES (in_insert_identity) RETURNING id INTO insert_identity_key;
-        END IF;
-    END IF;
-
-    INSERT INTO storagedata (
-                    record_id,
-                    create_time,
-                    storage_system_id,
-                    storage_share_id,
-                    storage_media_id,
-                    storage_class_id,
-                    file_count,
-                    directory_path_id,
-                    local_user_id,
-                    local_group_id,
-                    user_identity_id,
-                    group_identity_id,
-                    measure_time,
-                    valid_duration,
-                    resource_capacity_used,
-                    logical_capacity_used,
-                    insert_host_id,
-                    insert_identity_id,
-                    insert_time
-            )
-            VALUES (
-                    in_record_id,
-                    in_create_time,
-                    storage_system_key,
-                    storage_share_key,
-                    storage_media_key,
-                    storage_class_key,
-                    in_file_count,
-                    directory_path_key,
-                    local_user_key,
-                    local_group_key,
-                    user_identity_key,
-                    group_identity_key,
-                    in_measure_time,
-                    in_valid_duration,
-                    in_resource_capacity_used,
-                    in_logical_capacity_used,
-                    insert_host_key,
-                    insert_identity_key,
-                    in_insert_time
-                    )
-            RETURNING id INTO sr_key;
-
-    result[0] = in_record_id;
-    result[1] = sr_key;
-    RETURN result;
-
-END;
-$recordid_rowid$
-LANGUAGE plpgsql;
-
